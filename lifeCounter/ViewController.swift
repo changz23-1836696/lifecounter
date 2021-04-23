2fd33fc2e72f5ffb0030252225a9d36be18f2c92
changz23-1836696 <43832007+changz23-1836696@users.noreply.github.com>
2021-04-22 18:47:54 -0700

    Initial Commit

---
+++ lifeCounter/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  lifeCounter
+//
+//  Created by 曾畅 on 2021/4/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
